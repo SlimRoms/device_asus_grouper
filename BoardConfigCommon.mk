a56d7d5dfffab8e28be12886ed980df0b29ba2ad
Lars Greiss <kufikugel@googlemail.com>
2013-08-22 02:53:32 +0200

    Move selinux policy to common for tilapia
    so tilapia can use it as well
    
    Change-Id: I9ab3af20698e15de3510fb8307044ee547b0c89f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,3 +81,24 @@ BOARD_USES_GROUPER_MODULES := true
 
 # Required for CWM
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+BOARD_SEPOLICY_DIRS := \
+        device/asus/grouper/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+        file_contexts \
+        genfs_contexts \
+        app.te \
+        btmacreader.te \
+        device.te \
+        drmserver.te \
+        file.te \
+        gpsd.te \
+        init_shell.te \
+        keystore.te \
+        mediaserver.te \
+        rild.te \
+        sensors_config.te \
+        surfaceflinger.te \
+        system.te \
+        zygote.te
