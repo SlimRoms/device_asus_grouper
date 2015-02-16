0684550b1567d824fa917b821f7214a3e9a430fb
Josue Rivera <prbassplayer@gmail.com>
2015-02-15 17:14:03 -0700

    Grouper: Remove custom toolchain
      * Stock aosp kernel for now
      * Beside deprecated method anyways
    
    Change-Id: I13d713f2d9d7ad16099acbe0900aff0dbfb06f19
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,6 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/asus/grouper
 TARGET_KERNEL_CONFIG := slim_grouper_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-13.04/bin/arm-linux-gnueabihf-
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
